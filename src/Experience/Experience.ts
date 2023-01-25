5727f1d794247d3f7384b53862dba4d77cba4d98
LaupWing <loc_strict@hotmail.com>
2023-01-25 14:47:00 +0100

    added three

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,12 +1,14 @@
 import Debug from "./Utils/Debug"
+import * as THREE from "three"
 
 export default class Experience {
    canvas: HTMLCanvasElement
    debug: Debug
-   
+   scene: THREE.Scene
 
    constructor(canvas: HTMLCanvasElement){
       this.canvas = canvas
       this.debug = new Debug()
+      this.scene = new THREE.Scene()
    }
 }
