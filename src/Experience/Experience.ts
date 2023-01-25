c9dae7a9c5c488f3ce17c0a1cea3674ae169979b
LaupWing <loc_strict@hotmail.com>
2023-01-25 14:50:28 +0100

    ressize listener

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,14 +1,25 @@
 import Debug from "./Utils/Debug"
 import * as THREE from "three"
+import Sizes from "./Utils/Sizes"
 
 export default class Experience {
    canvas: HTMLCanvasElement
    debug: Debug
    scene: THREE.Scene
+   sizes: Sizes
 
    constructor(canvas: HTMLCanvasElement){
       this.canvas = canvas
       this.debug = new Debug()
       this.scene = new THREE.Scene()
+      this.sizes = new Sizes()
+
+      this.sizes.on("resize", () => {
+         this.resize()
+      })
+   }
+
+   resize(){
+      
    }
 }
