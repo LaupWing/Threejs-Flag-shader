eff476d683391c8516e1a6f24b7f4a46fad1b3a5
LaupWing <loc_strict@hotmail.com>
2023-01-25 14:58:09 +0100

    udpate

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,18 +1,21 @@
 import Debug from "./Utils/Debug"
 import * as THREE from "three"
 import Sizes from "./Utils/Sizes"
+import Camera from "./Camera"
 
 export default class Experience {
    canvas: HTMLCanvasElement
    debug: Debug
    scene: THREE.Scene
    sizes: Sizes
+   camera: Camera
 
    constructor(canvas: HTMLCanvasElement){
       this.canvas = canvas
       this.debug = new Debug()
       this.scene = new THREE.Scene()
       this.sizes = new Sizes()
+      this.camera = new Camera(this)
 
       this.sizes.on("resize", () => {
          this.resize()
@@ -20,6 +23,6 @@ export default class Experience {
    }
 
    resize(){
-      
+      this.camera.resize()
    }
 }
--- src/Experience/Renderer.ts
+++ src/Experience/Renderer.ts
@@ -1,5 +1,15 @@
+import Experience from "./Experience"
+import Sizes from "./Utils/Sizes"
+
 export default class Renderer {
-   constructor(){
-      
+   private experience: Experience
+   private canvas: HTMLCanvasElement
+   private sizes: Sizes
+   private scene; 
+
+   constructor(experience: Experience){
+      this.experience = experience
+      this.canvas = this.experience.canvas
+      this.sizes = this.experience.sizes
    }
 }
