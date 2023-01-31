324c21ccaa7c0222d7ea0aa98ef1f91586fc49c3
LaupWing <loc_strict@hotmail.com>
2023-01-31 14:12:33 +0100

    updat

--- src/Experience/Flag/Flag.ts
+++ src/Experience/Flag/Flag.ts
@@ -14,7 +14,16 @@ export default class Flag {
          fragmentShader,
          vertexShader
       })
+
       const mesh = new THREE.Mesh(this.geometry, this.material)
+      const count = this.geometry.attributes.position.count
+      const randoms = new Float32Array(count)
+      for (let i = 0; i < count; i ++){
+         randoms[i] = Math.random()
+      }
+
+      this.geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1))
+      console.log(this.geometry.attributes.position)
       experience.scene.add(mesh)
    }
 }
