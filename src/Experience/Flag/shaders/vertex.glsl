3da49378584926efddf1c8b2ae8718b3a7ed6347
LaupWing <loc_strict@hotmail.com>
2023-02-02 14:41:13 +0100

    udpate

--- src/Experience/Flag/shaders/vertex.glsl
+++ src/Experience/Flag/shaders/vertex.glsl
@@ -5,16 +5,18 @@ uniform vec2 uFrequency;
 uniform float uTime;
 
 attribute vec3 position;
-attribute float aRandom;
 attribute vec2 uv;
 
-varying float vRandom;
 varying vec2 vUv;
+varying float vEl
 
 void main(){
    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-   modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-   modelPosition.z += sin(modelPosition.y * uFrequency.y  + uTime) * 0.1;
+
+   float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+   elevation += sin(modelPosition.y * uFrequency.y  + uTime) * 0.1;
+
+   modelPosition.z += elevation;
 
    vec4 viewPosition = viewMatrix * modelPosition;
    vec4 projectedPosition = projectionMatrix * viewPosition;
